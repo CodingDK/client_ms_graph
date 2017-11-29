c315fcd75fba8943d4d8cd11ca1868d730857c16
CodingDK <CodingDK@users.noreply.github.com>
2017-11-29 09:53:08 +0100

    added signIn with ms account button, not working 100%

--- src/app.js
+++ src/app.js
@@ -1,8 +1,64 @@
+import * as Msal from "msal";
 require("../node_modules/@microsoft/microsoft-graph-client/lib/graph-js-sdk-web");
 
-require("./secrets");
+import { SECRETS } from "./secrets";
+var applicationConfig = {
+    clientID: SECRETS.CLIENT_ID,
+    graphScopes: ["user.read"]//, "mail.send"]
+};
 
-var client = MicrosoftGraph.Client.init({
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  var el = document.getElementById("btnLogin");
+  console.log("el?", el);
+  el.addEventListener("click", function eventClick(){
+    loginWithMS();
+  });
+}
+
+function loginWithMS(){
+  var logger = new Msal.Logger(loggerCallback, { level: Msal.LogLevel.Verbose, correlationId:'12345' }); // level and correlationId are optional parameters.
+  //Logger has other optional parameters like piiLoggingEnabled which can be assigned as shown aabove. Please refer to the docs to see the full list and their default values.
+
+  function loggerCallback(logLevel, message, piiLoggingEnabled) {
+      console.log(message);
+  }
+
+  var userAgentApplication = new Msal.UserAgentApplication(applicationConfig.clientID, null, authCallback, { logger: logger, cacheLocation: 'localStorage'}); //logger and cacheLocation are optional parameters.
+  //userAgentApplication has other optional parameters like redirectUri which can be assigned as shown above.Please refer to the docs to see the full list and their default values.
+  function authCallback(errorDesc, token, error, tokenType) {
+      if (token) {
+      }
+      else {
+          log(error + ":" + errorDesc);
+      }
+  }
+
+  userAgentApplication.loginPopup(applicationConfig.graphScopes).then(function (idToken) {
+      //Login Success
+      userAgentApplication.acquireTokenSilent(applicationConfig.graphScopes).then(function (accessToken) {
+          //AcquireToken Success
+          console.log("LOGGED IN! Hello World!");
+      }, function (error) {
+          //AcquireToken Failure, send an interactive request.
+          userAgentApplication.acquireTokenPopup(applicationConfig.graphScopes).then(function (accessToken) {
+              updateUI();
+          }, function (error) {
+              console.log(error);
+          });
+      })
+  }, function (error) {
+      console.log(error);
+  });
+}
+
+
+
+
+
+
+/*var client = MicrosoftGraph.Client.init({
 			debugLogging: true,
 			authProvider: function(done) {
 				done(null, SECRETS.ACCESS_TOKEN);
@@ -28,3 +84,4 @@ var client = MicrosoftGraph.Client.init({
 				const blobUrl = url.createObjectURL(rawResponse.xhr.response);
 				document.getElementById("profileImg").setAttribute("src", blobUrl);
 			});		
+      */
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    devtool: 'source-map',
     entry: "./src/app.js",
     output: {
         path: __dirname + "/public",
